b87f9c6db56213f6783a42e27f15efcfb39af60f
Chuma <chuma99@gmail.com>
2018-09-07 00:43:03 -0400

    f c
    again

--- fristProgram.py
+++ fristProgram.py
@@ -7,9 +7,9 @@ username = input("What's your name?\n")
 hometown = input("Hello, " +username+ "! Where are you from?\n")
 grade = input("Awesome, what grade are you in?\n")
 returner = input(grade+ " graders are the coolest! Were you a student at choate last year?")
-if returner = "yes":
+if returner == "yes":
  print("That's cool, " +username+ ". Congratulations on making it this far, and good luck for the rest of this year!")
-elif returner = "no":
+elif returner == "no":
  print("Glad that you've come to join us, " +username+ ". It was fun speaking with you, and I'm sure you'll meet many other" +grade+ " graders soon!")
 else:
  print("Okay cool!")
