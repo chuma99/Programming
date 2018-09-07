c667c10ed0ca89ccc9e6ec774e1d2bae5966943d
Chuma <chuma99@gmail.com>
2018-09-07 00:31:16 -0400

    Fourth Commit
    Homework Assignment

--- fristProgram.py
+++ fristProgram.py
@@ -1,6 +1,12 @@
 # comment
+# homework is to write a program that has a conversation with you
 # python3 vs python
 
 print("Hello, world!")
 username = input("What's your name?\n")
-print("Hello, " +username+ "!")
+hometown = input("Hello, " +username+ "! Where are you from?\n")
+grade = input("Awesome, what grade are you in?\n")
+returner = input(grade+ " graders are the coolest! Were you a student at choate last year?")
+if returner = "yes" print("That's cool," +username+ ". Congratulations on making it this far, and good luck for the rest of this year!")
+else if returner = "no" print("Glad that you've come to join us, " +username+ ". It was fun speaking with you, and I'm sure you'll meet many other" +grade+ " graders soon!")
+else print("Okay cool!")
