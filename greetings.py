7fc97d689b4841d29aadcec02248dea768554863
Chuma <chuma99@gmail.com>
2018-09-10 11:59:27 -0400

    Third Commit

--- greetings.py
+++ greetings.py
@@ -1,2 +1,8 @@
 import sys
-print("Greetings, ", sys.argv[1] + "!")
+#https://www.pythonforbeginners.com/system/python-sys-argv where I found the value of position 0.
+if len(sys.argv) == 1:
+print("Greetings, " + sys.argv[1] + "!")
+elif len(sys.argv) == 2:
+ print("Greetings, " + sys.argv[1] + "and " + sys.argv[2] + "!")
+elif len(sys.argv) == 3:
+ print("Greetings, " + sys.argv[1]+ ", " +sys.argv[2]+ ", and " + sys.argv[3] + "!")
