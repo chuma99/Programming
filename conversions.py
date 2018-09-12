5561c8ba0ba15ab9a11d9c0428e5c766c2f94938
Chuma <chuma99@gmail.com>
2018-09-12 11:01:52 -0400

    5

--- conversions.py
+++ conversions.py
@@ -1,4 +1,8 @@
 #this first conversion will be celcius to kelvin
-celsius = int(input("Please enter degrees in celsius: "))
+celsius = float(input("Please enter degrees in celsius: "))
 kelvin = celsius + 273.15
 print("The temperature in Kelvin:",kelvin)
+#this first conversion will be celsius to fahrenheit
+celsius = float(input("Please enter degrees in celsius: "))
+fahrenheit = (1.8 * celsius) + 32
+print("The temperature in Fahrenheit:",fahrenheit)
