cd89ec4e784d0468836efb35b39675cdfee0e474
Chuma <chuma99@gmail.com>
2018-09-12 10:53:56 -0400

    4

--- conversions.py
+++ conversions.py
@@ -1,4 +1,4 @@
 #this first conversion will be celcius to kelvin
 celsius = int(input("Please enter degrees in celsius: "))
 kelvin = celsius + 273.15
-print("The temperature in Kelvin: " + str(kelvin))
+print("The temperature in Kelvin:",kelvin)
