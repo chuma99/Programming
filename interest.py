7ebdc4ef8a72f318758e918fc48299ab0bca074d
Chuma <chuma99@gmail.com>
2018-09-14 10:22:13 -0400

    p6

--- interest.py
+++ interest.py
@@ -3,5 +3,5 @@ i = float(input("Input your investment: "))
 r = float(input("Input your interest rate: "))
 y = float(input("How many years would you like to invest for?: "))
 
-A = float(i*(m.e)*(r*y))
+A = float(i*(m.e)**(r*y))
 print("Future investment value:", A)
