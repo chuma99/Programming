e71ed821caafdb045dd3ace08192af0cc7c6d93b
Chuma <chuma99@gmail.com>
2018-09-19 11:16:47 -0400

    fix5

---
+++ whilecheck.py
@@ -0,0 +1,13 @@
+while True:
+	num = int(input("Pick a number between 1 and 5: "))
+	integer()
+	between()
+	print("Good Job")	
+
+def integer()
+	while type(num) != int:
+		num = int(input("That's not right. Pick an integer between 1 and 5: "))	
+
+def between()
+	while num>5 and num<1:
+		print("That's not right. Pick a number BETWEEN 1 and 5: ")
