e81eb22d8490c1de043d2a48443129066a62acdc
Chuma <chuma99@gmail.com>
2018-09-19 10:25:30 -0400

    Save1

--- guessinggame.py
+++ guessinggame.py
@@ -1,15 +1,16 @@
 import math as m
-num = int(m.rand() * 100)
-def start():
 
+def start():
+	num = int(m.rand() * 100)
+	def guess():
 	response = int(input("Guess an integer from 0 to 99:"))
 		if response == num:
-			answer = input("Correct! You win! Would you like to play again? Y/N \n")
+			answer = lower(input("Correct! You win! Would you like to play again? Y/N \n"))
+			if answer == "y":
+				start()
+			elif answer == "n":
+				print("Thanks for playing the game!")
 		else:
 			answer = input("Incorrect. Guess again!")
-	elif response == "2":
-		map()
-	else:
-		print("You must type 1 or 2. Please try again.")
-		start()
+			guess()
 start()
