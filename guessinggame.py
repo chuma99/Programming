86e22e53f7f66bcefbc08479d4a257e2f5a293e5
Chuma <chuma99@gmail.com>
2018-09-19 10:43:45 -0400

    fix1

--- guessinggame.py
+++ guessinggame.py
@@ -1,16 +1,26 @@
 import math as m
 
+start()
+
 def start():
 	num = int(m.rand() * 100)
-	def guess():
+	guess()
+
+def guess():
 		response = int(input("Guess an integer from 0 to 99:"))
-			if response == num:
-				answer = lower(input("Correct! You win! Would you like to play again? Y/N \n"))
-				if answer == "y":
-					start()
-				elif answer == "n":
-					print("Thanks for playing the game!")
+		if response == num:
+			print("Correct! You win!\n")
+			restart()
+		else:
+			answer = input("Incorrect. Guess again!")
+			guess()
+
+def restart():
+			answer = lower(input("Would you like to play again? Y/N \n"))
+			if answer == "y":
+				start()
+			elif answer == "n":
+				print("Thanks for playing the game!")
 			else:
-				answer = input("Incorrect. Guess again!")
-				guess()
-start()
+				print("Incorrect input. Try again")
+				restart()
