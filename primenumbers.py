d2393895dca13fc8fcd76f196c64293568288db1
Chuma <chuma99@gmail.com>
2018-09-28 07:26:12 -0400

    #

---
+++ primenumbers.py
@@ -0,0 +1,24 @@
+import random
+
+randoms=[]
+for i in range (10):
+    randoms.append(random.randrange(1,101,1))
+print(randoms)
+
+
+
+#generate a list of prime numbers from one through ten
+#should print 1,2,3,5,7
+
+#a = [1,2,3,4,5,6,7,8,9,10]
+#for i in range :
+#	one = a[i]
+#	if a[i]/2 isinstance(one, int)
+#		a.remove(a[i])
+#	elif a[i]/3 isinstance(one, int)
+#		a.remove(a[i])
+#	elif a[i]/5 isinstance(one, int)
+#		a.remove(a[i])
+#	elif a[i]/7 isinstance(one, int)
+#		a.remove(a[i])
+#print(a)
