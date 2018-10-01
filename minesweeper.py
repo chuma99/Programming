efeec7e5ee6db82b1446fa2d91a55f845caa3494
Chuma <chuma99@gmail.com>
2018-10-01 02:07:42 -0400

    2

--- minesweeper.py
+++ minesweeper.py
@@ -1,22 +1,24 @@
 def createBoard (rows, cols, mines):
 
-    board = []
-    #set the range to the number of rows and columns provided +1
+board = []
+for r in range (0,rows+1):
+    board.append([])
+    for c in range (0,cols+1):
+        board[r].append("C ")
+
+num_mines=0
+
+while num_mines < mines :
+    x = random.choice(board)
+    y = random.choice(x)
+    if y == "C ":
+        x[y]= "C*"
+
+
+        num_mines = num_mines+1
+
+createBoard()
+
+#set the range to the number of rows and columns provided +1
     #for every row, create a row and a column
-    for r in range (0,rows+1):
-        board.append([])
-        for c in range (0,cols+1):
-            board[r].append("C ")
-
-    #set # of mines to 0
-
-    num_mines=0
-
-    while num_mines < mines :
-        x = random.choice(board)
-        y = random.choice(x)
-        if y == "C ":
-            x[y]= "C*"
-
-
-            num_mines = num_mines+1
+     #set # of mines to 0
