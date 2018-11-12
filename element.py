2e01bb704e50de7d460b8cd7933d1304e2af5735
Chuma <chuma99@gmail.com>
2018-11-12 01:59:08 -0500

    more working print

--- element.py
+++ element.py
@@ -28,16 +28,15 @@ class PeriodicTable:
 	def __init__(self, table):
 		self.table = pd.read_csv(table)
 		i = 1;
-		self.elementList = list(zip(*self.table))
+		self.elementList = []
 		#holdonwhile (i < 103):
 			#self.elementList.append(Element("Hydrogen", 1, "H", 1.01))
 			#self.elementList.append(Element("Helium", 12, "He", 1.07))
 			#holdonself.elementList.append(Element(table[i][0], table[i][1], table[i][2], table[i][3]))
 			#holdoni+=1
- 		#next(self.table)
 
-		#.    for line in self.table:
-			#.    self.elementList.append(line)
+		for list in self.table:
+			self.elementList.append(list)
 
 	def __str__(self):
 		return str()
@@ -46,10 +45,14 @@ class PeriodicTable:
 	def getElementList(self):
 		return self.elementList
 
+	def getTable(self):
+		return self.table
 #data = pd.read_csv("elements.csv")
+
 #print(data)
 
 pt = PeriodicTable("elements.csv")
 #pt.printTable()
 print(pt.getElementList())
+print(pt.getTable())
 # periodicTable.append(Element("Hydrogen", 1, "H", 1.01))
